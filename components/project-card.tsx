14ea664e800497e388029ab238519f5ca765efd6
Megan Beebe <meganbeebe@Megans-Air.attlocal.net>
2023-05-11 15:07:51 -0400

    Finish projects page

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -1,20 +1,27 @@
-import { UserCircleIcon } from '@heroicons/react/24/outline'
-import Link from 'next/link'
+import Image from "next/image"
 
-function ProjectCard() {    
+type ProjectCardProps = {
+    imageSrc: string,
+    imageAlt: string,
+    title: string,
+    description: string,
+    attributes: Array<string>
+}
+
+function ProjectCard({imageSrc, imageAlt, title, description, attributes}: ProjectCardProps) {    
     return (  
-        <div role="project-card-1" className="max-w-sm w-full rounded-xl overflow-hidden shadow-lg">
-            <img className="w-full" src="/splash-screen.png" alt="meganlott.com" />
+        <div role="project-card" className="max-w-sm w-full rounded-xl overflow-hidden shadow-lg">
+            <Image height={250} width={500} src={imageSrc} alt={imageAlt} />
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">meganlott.com</div>
+                <div className="font-bold text-xl mb-2">{title}</div>
                 <p className="text-gray-700 text-base">
-                This is it! This portfolio site was designed and built by yours truly.
+                    {description}
                 </p>
             </div>
             <div className="px-6 pt-4 pb-2">
-                <span className="inline-block bg-light-gray rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">TypeScript</span>
-                <span className="inline-block bg-light-gray rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">Next.js</span>
-                <span className="inline-block bg-light-gray rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">Tailwind CSS</span>
+                {attributes.map((attr: string, index: number) => (
+                    <span key={index} className="inline-block bg-light-gray rounded-full px-3 py-1 text-sm font-semibold text-black mr-2 mb-2">{attr}</span>
+                ))}
             </div>
         </div>
     )
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -6,23 +6,56 @@ export default function Projects() {
 		<div 
 		role="projects-page"
         id="projects"
-		className="w-full h-full bg-white"
+		className="w-full h-full bg-white flex flex-col"
         >
 			<div role="header" className="h-40 pt-14 bg-heading bg-no-repeat bg-center bg-contain text-center">
 				<p className="text-heading text-white text-stroke">PROJECTS</p>
 			</div>
-			<div role="cards" className="w-full grid grid-rows-1 grid-flow-col gap-0 pt-20 justify-items-center">
-				<div role="left-arrow" className="flex items-center">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-20 h-20 stroke-light-gray">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-					</svg>
-				</div>
-				<ProjectCard />
-				<ProjectCard />
-				<div role="right-arrow" className="flex items-center">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-20 h-20 stroke-light-gray">
-						<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-					</svg>
+			<div className="flex flex-col flex-1 justify-center">
+				<div role="cards" className="h-max-full w-full grid grid-rows-1 grid-flow-col gap-0 pt-4 justify-items-center">
+					<div role="left-arrow" className="flex items-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-20 h-20 stroke-light-gray">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+						</svg>
+					</div>
+					<ProjectCard 
+						imageSrc="/homepage.png"
+						imageAlt="Homepage of meganlott.com" 
+						title="meganlott.com" 
+						description="This is it! This portfolio site was designed and built by yours truly."
+						attributes={[
+							"TypeScript", 
+							"React",
+							"Next.js", 
+							"Tailwind CSS",
+							"AWS Lightsail"
+						]}
+					/>
+					{/* <ProjectCard 
+						imageSrc=""
+						imageAlt="HomeTeam by ReflexAI"
+						title="HomeTeam" 
+						description="A responsive web application to offer education and resources to military veterans looking to support their colleagues experiencing mental health crises."
+						attributes={[
+							"TypeScript", 
+							"React", 
+							"Next.js",
+							"Docker", 
+							"MongoDB",
+							"TailwindCSS", 
+							"Auth0", 
+							"Swagger", 
+							"Terraform",  
+							"Jest",
+							"Google Cloud Platform",
+							"Contentful"
+						]}
+					/> */}
+					<div role="right-arrow" className="flex items-center">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-20 h-20 stroke-light-gray">
+							<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+						</svg>
+					</div>
 				</div>
 			</div>
 		</div>
