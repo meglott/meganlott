17be3c872ad887a320572e41a2361cfeb63ad41f
Megan Beebe <meganbeebe@Megans-Air.attlocal.net>
2023-05-11 14:11:16 -0400

    Finish header and footer

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,25 +1,35 @@
 import { UserCircleIcon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import Link from 'next/link'
+import linkedInMark from "../../public/linkedin-mark.svg";
+import githubMark from "../../public/github-mark.svg";
 
 function FooterComponent() {    
     return (  
         <nav className="sticky h-24 top-0 left-0 w-full bg-white border-t border-light-gray">
             <div className="flex w-full h-full py-5 px-10 justify-between items-center">
                 <div className="flex items-center pl-10 gap-5">
-                    <p className="text-sm font-roboto font-normal">© 2023 Megan Lott</p>
+                    <p className="text-sm font-roboto font-normal">© 2023 Megan Lott LLC</p>
                 </div>
                 <div className="flex justify-end items-center pr-10 gap-10">
-                    <Link href="#home" className="uppercase text-xl font-roboto font-normal">
+                    <Link href="https://www.linkedin.com/in/megan-lott-35322284/">
                         <Image 
-                            src={'../../public/linkedin-mark.svg'}
-                            width={100}
-                            height={100}
+                            priority
+                            src={linkedInMark}
+                            width={30}
+                            height={30}
                             alt="LinkedIn Logo"
                         />
-
                     </Link>
-                    <Link href="#about" className="uppercase text-xl font-roboto font-normal">About</Link>
+                    <Link href="https://github.com/meglott">
+                    <Image 
+                            priority
+                            src={githubMark}
+                            width={30}
+                            height={30}
+                            alt="GitHub Logo"
+                        />
+                    </Link>
                 </div>
             </div>
         </nav>    
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,4 +1,5 @@
 import { UserCircleIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
 import Link from 'next/link'
 
 function HeaderComponent() {    
@@ -6,7 +7,12 @@ function HeaderComponent() {
         <nav className="sticky h-24 top-0 left-0 w-full bg-white border-b border-light-gray">
             <div className="flex w-full h-full py-5 px-10 justify-between items-center">
                 <div className="flex items-center pl-10 gap-5">
-                    <UserCircleIcon className="h-14 w-14" />
+                    <Image
+                        src="/../public/headshot.png"
+                        height={60}
+                        width={60}
+                        alt="Headshot of Megan Lott"
+                    />
                     <p className="uppercase text-xl font-roboto font-normal">Megan Lott</p>
                 </div>
                 <div className="flex justify-end items-center pr-10 gap-10">
