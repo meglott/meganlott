46656969f7babf9317b122eb280905ffdfd08adc
Megan Lott <mbeebe6@gatech.edu>
2023-05-19 10:57:11 -0400

    Update project page

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -6,15 +6,17 @@ function HeaderComponent() {
     return (  
         <nav className="sticky h-24 top-0 left-0 w-full bg-white border-b border-light-gray">
             <div className="flex w-full h-full py-5 px-10 justify-between items-center">
-                <div className="flex items-center pl-10 gap-5">
-                    <Image
-                        src={"/headshot.png"}
-                        height={60}
-                        width={60}
-                        alt="Headshot of Megan Lott"
-                    />
-                    <p className="uppercase text-xl font-roboto font-normal">Megan Lott</p>
-                </div>
+                <Link href="#home">
+                    <div className="flex items-center pl-10 gap-5">
+                        <Image
+                            src={"/headshot.png"}
+                            height={60}
+                            width={60}
+                            alt="Headshot of Megan Lott"
+                        />
+                        <p className="uppercase text-xl font-roboto font-normal">Megan Lott</p>
+                    </div>
+                </Link>
                 <div className="flex justify-end items-center pr-10 gap-10">
                     <Link href="#home" className="uppercase text-xl font-roboto font-normal">Home</Link>
                     <Link href="#about" className="uppercase text-xl font-roboto font-normal">About</Link>
