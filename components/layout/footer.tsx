50417443a8db6c6591177fd2233d88b42589ffaf
Megan Lott <mbeebe6@gatech.edu>
2023-05-21 20:31:22 -0400

    Adjust header and footer for all size screens

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -7,11 +7,11 @@ import githubMark from "../../public/github-mark.svg";
 function FooterComponent() {    
     return (  
         <nav className="sticky h-18 top-0 left-0 w-full bg-white border-t border-light-gray">
-            <div className="flex w-full h-full py-5 px-10 justify-between items-center">
-                <div className="flex items-center pl-10 gap-5">
+            <div className="flex w-full h-full py-5 px-5 md:px-10 justify-between items-center">
+                <div className="flex items-center gap-5">
                     <p className="text-sm font-roboto font-normal">© 2023 Megan Lott LLC</p>
                 </div>
-                <div className="flex justify-end items-center pr-10 gap-10">
+                <div className="flex justify-end items-center gap-5 md:gap-10">
                     <Link href="https://www.linkedin.com/in/megan-lott-35322284/" rel="noopener noreferrer" target="_blank">
                         <Image 
                             priority
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,23 +1,39 @@
-import { UserCircleIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon } from '@heroicons/react/24/outline'
 import Image from 'next/image'
 import Link from 'next/link'
 
 function HeaderComponent() {    
     return (  
         <nav className="sticky h-24 top-0 left-0 w-full bg-white border-b border-light-gray">
-            <div className="flex w-full h-full py-5 px-10 justify-between items-center">
-                <Link href="#home">
-                    <div className="flex items-center pl-10 gap-5">
+            <div className="flex w-full h-full py-5 px-5 md:px-10 justify-between items-center">
+                {/* Large screens: photo and name in same div */}
+                <Link href="#home" className='hidden lg:flex'>
+                    <div className="hidden lg:flex lg:items-center lg:pl-10 lg:gap-5">
+                        {/* Headshot */}
                         <Image
                             src={"/headshot.png"}
                             height={60}
                             width={60}
                             alt="Headshot of Megan Lott"
                         />
+                        {/* Name */}
                         <p className="uppercase text-xl font-roboto font-normal">Megan Lott</p>
                     </div>
                 </Link>
-                <div className="flex flex-row justify-end items-center pr-10 gap-10">
+                {/* Small screens: phone and name separate */}
+                <div className='visible lg:hidden'>
+                    <Image
+                        src={"/headshot.png"}
+                        height={60}
+                        width={60}
+                        alt="Headshot of Megan Lott"
+                    />
+                </div>
+                <div className='visible lg:hidden'>
+                    <p className="uppercase text-xl font-roboto font-normal">Megan Lott</p>
+                </div>
+                {/* Links */}
+                <div className="hidden lg:visible lg:flex lg:flex-row lg:justify-end lg:items-center lg:pr-10 lg:gap-10">
                     <Link href="#home" className="uppercase text-xl font-roboto font-normal">
                         <div className='flex items-center h-12 hover:border-b-4 hover:border-button hover:mb-[-4px]'>
                             Home
@@ -38,6 +54,9 @@ function HeaderComponent() {
                             Contact
                         </div>
                     </Link>
+                </div>
+                <div className="visible h-9 w-9 pr-10 gap-10 lg:hidden">
+                    <Bars3Icon fill='black' height={36} width={36} />
                 </div>
             </div>
         </nav>    
