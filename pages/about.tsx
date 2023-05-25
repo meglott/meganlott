174531cdaaa7630fb090cb25c6be038e5d7e9e98
Megan Lott <mbeebe6@gatech.edu>
2023-05-25 15:03:52 -0400

    Add polaroid to about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import AboutFile from "../components/about-file";
 
 export default function About() {
@@ -5,13 +6,48 @@ export default function About() {
 		<div 
             role="about-page"
             id="about"
-            className="flex-1 w-full h-full bg-light-green snap-start overflow-y-auto"
+            className="w-full h-full flex flex-col lg:flex-row justify-between md:justify-center items-center bg-light-green overflow-y-auto"
         >
-            <div className="flex flex-col lg:flex-row lg:gap-4 h-full justify-center items-center">
-                <div className="w-4/5 lg:w-2/5">
-                    Left Side 
+            <div className="flex flex-col w-full lg:flex-row gap-8 lg:gap-32 h-full justify-center items-center overflow-y-auto">
+                <div className="w-fit pt-8 lg:pt-0">
+                    <div role="lg-polaroid" className="hidden lg:flex flex-col pt-4 px-4 gap-3 -rotate-12 items-center w-fit h-60 lg:h-96 bg-white border border-black drop-shadow-polaroid">
+                        <div role="thumbtack" className="absolute -top-2 left-32">
+                            <Image 
+                                className="drop-shadow-polaroid"
+                                src={"/thumbtack.png"}
+                                height={60}
+                                width={60}
+                                alt="thumbtack"
+                            />
+                        </div>
+                        <Image
+                            src={"/about-me.png"}
+                            height={300}
+                            width={300}
+                            alt="Megan with her dog Titan"
+                        />
+                        <p className="text-2xl font-Permanent-Marker">Florida, 2018</p>
+                    </div>
+                    <div role="sm-polaroid" className="lg:hidden flex flex-col pt-2 px-2 gap-2 -rotate-12 items-center w-fit h-64 bg-white border border-black drop-shadow-polaroid">
+                        <div role="thumbtack" className="absolute -top-2 left-20">
+                            <Image 
+                                className="drop-shadow-polaroid"
+                                src={"/thumbtack.png"}
+                                height={40}
+                                width={40}
+                                alt="thumbtack"
+                            />
+                        </div>
+                        <Image
+                            src={"/about-me.png"}
+                            height={200}
+                            width={200}
+                            alt="Megan with her dog Titan"
+                        />
+                        <p className="text-lg font-Permanent-Marker">Florida, 2018</p>
+                    </div>
                 </div>
-                <div className="flex w-4/5 lg:w-2/5 h-4/5 lg:h-full justify-center items-center">
+                <div className="flex w-4/5 lg:w-2/5 h-4/5 min-h-fit lg:h-full justify-center items-center">
                     <AboutFile />
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,12 @@ module.exports = {
         'subtitle-md': ['32px', {lineHeight: '42px', fontWeight: '400'}],
         heading: ['80px', {lineHeight: '56px', fontWeight: '400'}],
         'label-lg': ['20px', {lineHeight: '32px', fontWeight: '400'}],
+      },
+      fontFamily: {
+        'permanent-marker': ['Permanent Marker', 'sans-serif']
+      },
+      dropShadow: {
+        'polaroid': '-5px 5px 5px rgba(0, 0, 0, 0.6)'
       }
     },
   },
