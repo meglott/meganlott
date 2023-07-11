131e4d2b53939f0a95df11b0669cf9e5db7354c2
Meg Lott <meg.d.lott@gmail.com>
2023-07-11 13:12:03 -0400

    Add amplify config

---
+++ src/graphql/subscriptions.ts
@@ -0,0 +1,43 @@
+/* tslint:disable */
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateCandidate = /* GraphQL */ `
+  subscription OnCreateCandidate(
+    $filter: ModelSubscriptionCandidateFilterInput
+  ) {
+    onCreateCandidate(filter: $filter) {
+      id
+      name
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateCandidate = /* GraphQL */ `
+  subscription OnUpdateCandidate(
+    $filter: ModelSubscriptionCandidateFilterInput
+  ) {
+    onUpdateCandidate(filter: $filter) {
+      id
+      name
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteCandidate = /* GraphQL */ `
+  subscription OnDeleteCandidate(
+    $filter: ModelSubscriptionCandidateFilterInput
+  ) {
+    onDeleteCandidate(filter: $filter) {
+      id
+      name
+      email
+      createdAt
+      updatedAt
+    }
+  }
+`;
