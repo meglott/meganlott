34b22517932422583f8a845111dc4842eb393436
Meg Lott <meg.d.lott@gmail.com>
2023-07-11 13:45:29 -0400

    Update sendgrid

--- pages/api/sendgrid.tsx
+++ pages/api/sendgrid.tsx
@@ -8,22 +8,24 @@ if (process.env.SENDGRID_API_KEY) {
 }
 
 async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
+
+  const message = {to: "info@meganlott.com",
+  from: "info@meganlott.com",
+  cc: `${req.body.email}`,
+  subject: "[MEGANLOTT] New Inquiry",
+  html: `<div style="background-color:gray; width: 100%; height: 100%">
+        <div style="background-color:white; text-align:center; width:80%; height:80%; margin:auto; padding-top:15px;">
+            <h1 style="color:#45b09a">MEGAN LOTT LLC<h1>
+            <p style="font-size: 12px">Name:  ${req.body.firstname} ${req.body.lastname}</p>
+            <p style="font-size: 12px">Email: ${req.body.email}</p>
+            <p style="font-size: 12px"><b>Phone Number:</b> ${req.body.phonenumber}</p>
+            <p style="font-size: 12px"><b>Message:</b> <br />${req.body.message}</p>
+        </div>
+    </div>`};
+
   try {
-    await sendgrid.send({
-      to: "info@meganlott.com",
-      from: "info@meganlott.com",
-      cc: `${req.body.email}`,
-      subject: "[MEGANLOTT] New Inquiry",
-      html: `<div style="background-color:gray; width: 100%; height: 100%">
-            <div style="background-color:white; text-align:center; width:80%; height:80%; margin:auto; padding-top:15px;">
-                <h1 style="color:#45b09a">MEGAN LOTT LLC<h1>
-                <p style="font-size: 12px">Name:  ${req.body.firstname} ${req.body.lastname}</p>
-                <p style="font-size: 12px">Email: ${req.body.email}</p>
-                <p style="font-size: 12px"><b>Phone Number:</b> ${req.body.phonenumber}</p>
-                <p style="font-size: 12px"><b>Message:</b> <br />${req.body.message}</p>
-            </div>
-        </div>`,
-    });
+    const sent = await sendgrid.send(message);
+    console.log("Sent: " + sent);
   } catch (error: any) {
     console.log(error);
     return res.status(error.statusCode || 500).json({ error: error.message });
